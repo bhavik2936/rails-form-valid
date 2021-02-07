21541071dc797e224f97689fa2777b0f539786a7
Bhavik Parmar <bhavik2936@gmail.com>
2021-02-07 18:30:28 +0530

    Employee creation completed

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,6 +8,21 @@ class EmployeesController < ApplicationController
   end
 
   def new
-
+    @employee = Employee.new
   end
+
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to @employee
+    else
+      render :new
+    end
+  end
+
+  private
+    def employee_params
+      params.require(:employee).permit(:name, :address)
+    end
 end
