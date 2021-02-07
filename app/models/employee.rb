49c40a56c5e4e78b4aaa1cde9f5ae691059ecdec
Bhavik Parmar <bhavik2936@gmail.com>
2021-02-07 22:49:16 +0530

    employee validations added

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,8 @@
 class Employee < ApplicationRecord
+  validates :name, length: { minimum: 2, maximum: 30 }, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, presence: true
+  validates :dob, presence: true
+  validates :mobile, numericality: true, length: { is: 10 }, presence: true
+  validates :designation, presence: true
+  validates :salary, numericality: true, presence: true
 end
