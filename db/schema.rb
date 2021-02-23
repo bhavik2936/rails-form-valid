d7838fb20af7adce8f2aa35eb09c7df589050416
Bhavik Parmar <bhavik2936@gmail.com>
2021-02-23 09:30:49 +0530

    manager model migration performed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_120217) do
+ActiveRecord::Schema.define(version: 2021_02_22_105336) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2021_02_07_120217) do
     t.string "mobile"
     t.string "designation"
     t.integer "salary"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "managers", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
